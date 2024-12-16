0cfc3f9a3244d41e531a684f29009bf70fc9ea9d
anishmhaske02 <anishmhaske02@gmail.com>
2024-12-16 16:32:38 +0530

    assignmentQ4 updated

--- python_asng01/asgn01_Q4.py
+++ python_asng01/asgn01_Q4.py
@@ -1,12 +1,16 @@
-def avg_fn(p1:int, p2:int, p3:int):
-    result = (p1 + p2 + p3) / 3
-    return result
+def max_fn(p1:int, p2:int, p3:int):
+    if p1 > p2:
+        if p1 > p3:
+            return p1
+    elif p2 > p3:
+        return p2
+    else: 
+        return p3
 
 a = int(input("Enter num1: "))
 b = int(input("Enter num2: "))
 c = int(input("Enter num3: "))
 
-avg = avg_fn(a, b, c)
+max = max_fn(a, b, c)
 
-print(f"Average = {avg}")   
-print(f"Average = {avg}")   
+print(f"Max number = {max}")   
